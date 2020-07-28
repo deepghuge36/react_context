d6c388422f1bf22f47d79897a84bc440b7a485ed
DIPAK GHUGE <deepghuge@gmail.com>
2020-07-29 04:20:44 +0530

    Task added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Task from './Components/Task'
 function App() {
   return (
     <div>
-    <h1>d</h1>
+<Task/>
     </div>
   );
 }
---
+++ src/Components/Task.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Task extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Task</h2>
+      </div>
+    )
+  }
+}
